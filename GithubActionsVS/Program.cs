3d687917f805bfa41eceb6a6eaae7c81b83ac14a
Tayrific <103253195+Tayrific@users.noreply.github.com>
2024-07-24 14:01:22 +0100

    all into 1 line

--- GithubActionsVS/Program.cs
+++ GithubActionsVS/Program.cs
@@ -1,13 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () =>
-{
-    // Original response
-    var response = "Hello World! This is for GitHub Actions";
+app.MapGet("/", () => "Hello World! This is for GitHub Actions\nThis is another Example");
 
-    // New line added
-    response += "\nThis is another Example";
-
-    return response;
-});
+app.Run();
